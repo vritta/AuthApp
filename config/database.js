a373d395cdc8d0a15893f8dadf639bfa228eb654
vritta <anuhartripathi15@gmail.com>
2024-06-02 21:08:53 +0530

    created router, model and started server

--- config/database.js
+++ config/database.js
@@ -1,16 +1,18 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
-
+require("dotenv").config;
 const DATABASE_URL = process.env.DATABASE_URL;
-exports.connect = ()=>{
-    mongoose.connect(DATABASE_URL, {
+
+connect = ()=>{
+    mongoose.connect(process.env.DATABASE_URL, {
         useNewUrlParser:true,
-        useUnifiedTopology:true
+        useUnifiedTopology:true,
     })
     .then(()=>{console.log("Database is connected successfully")})
-    .catch(err){
+    .catch((err)=>{
         console.log("DB Connection issues");
         console.error(err);
         process.exit(1);
-    }
+    });
 }
+
+module.exports =  connect;
--- index.js
+++ index.js
@@ -5,3 +5,15 @@ require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
+// require("./config/database").connect();
+const dbConnect = require("./config/database");
+dbConnect();
+
+//route import and mount
+const user = require("./routes/user");
+app.use("./app/v1/", user);
+
+//activate server
+app.listen(PORT, ()=>{
+    console.log(`App is listening at ${PORT}`);
+})
