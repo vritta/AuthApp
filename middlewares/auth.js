6632093ff076827e8dc3c5dc13c4278f0f21f7fd
vritta <anuhartripathi15@gmail.com>
2024-06-05 18:50:16 +0530

    adding middlewares

---
+++ middlewares/auth.js
@@ -0,0 +1,72 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+exports.auth = (req,res,next)=>{
+    try{
+        // extract JWT token
+        // PENDING: other ways to fetch token
+        const token = req.body.token ;
+        if(!token){
+            return res.status(401).json({
+                success:false,
+                message:'Token Missing',
+            });
+        }
+
+        // verify the token
+        try{
+            const decode = jwt.verify(token, process.env.JWT_SECRET);
+            console.log(decode);
+
+            req.user = decode;
+        } catch(err){
+            return res.status(401).json({
+                success: false,
+                message: "Token is Invalid"
+            });
+        }
+        next();
+    } catch(err){
+        return res.status(401).json({
+            success: false,
+            message:"Something went wrong, while verifying the token",
+        });
+    }
+}
+
+exports.isStudent = ()=>(req,res,next)=>{
+    try{
+        if(req.user.role != "Student"){
+            return res.status(401).json({
+                success:false,
+                message:'This is a protected route for students',
+            });
+        }
+        const token = req.body.token ;
+        if(!token){
+            return res.status(401).json({
+                success:false,
+                message:'Token Missing',
+            });
+        }
+
+        // verify the token
+        try{
+            const decode = jwt.verify(token, process.env.JWT_SECRET);
+            console.log(decode);
+
+            req.user = decode;
+        } catch(err){
+            return res.status(401).json({
+                success: false,
+                message: "Token is Invalid"
+            });
+        }
+        next();
+    } catch(err){
+        return res.status(401).json({
+            success: false,
+            message:"Something went wrong, while verifying the token",
+        });
+    }
+}
