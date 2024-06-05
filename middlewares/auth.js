96baacd5ffa0b342e7aa52992f4337bfc3bf64ae
vritta <anuhartripathi15@gmail.com>
2024-06-05 21:56:17 +0530

    adding middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -20,6 +20,7 @@ exports.auth = (req,res,next)=>{
 
             req.user = decode;
         } catch(err){
+            console.error(err)
             return res.status(401).json({
                 success: false,
                 message: "Token is Invalid"
@@ -42,31 +43,28 @@ exports.isStudent = ()=>(req,res,next)=>{
                 message:'This is a protected route for students',
             });
         }
-        const token = req.body.token ;
-        if(!token){
+        next();
+    } catch(err){
+        return res.status(500).json({
+            success: false,
+            message:"User role is not matching",
+        });
+    }
+}
+
+exports.isAdmin = (req, res, next)=>{
+    try{
+        if(req.user.role != "Admin"){
             return res.status(401).json({
                 success:false,
-                message:'Token Missing',
-            });
-        }
-
-        // verify the token
-        try{
-            const decode = jwt.verify(token, process.env.JWT_SECRET);
-            console.log(decode);
-
-            req.user = decode;
-        } catch(err){
-            return res.status(401).json({
-                success: false,
-                message: "Token is Invalid"
+                message:'This is a protected route for students',
             });
         }
         next();
     } catch(err){
-        return res.status(401).json({
+        return res.status(500).json({
             success: false,
-            message:"Something went wrong, while verifying the token",
+            message:"User role is not matching",
         });
     }
 }
